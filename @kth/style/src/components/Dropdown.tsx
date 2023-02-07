529305b9ce8ce61f98c4ac72d405efe663e9102e
Carlos Saito <carlos@saito.style>
2023-02-07 17:17:42 +0100

    Rename component file

---
+++ @kth/style/src/components/Dropdown.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+function ChevronDown() {
+  return <div>TODO</div>;
+}
+
+export function DropdownButton() {
+  return (
+    <button className="kth-0-button">
+      <span>Administrera kurs</span>
+      <ChevronDown />
+    </button>
+  );
+}
+
+export function DropdownContainer() {
+  return <div>TODO</div>;
+}
+
+export function DropdownGroup() {
+  return <div>TODO</div>;
+}
+
+export function DropdownElement() {
+  return <div>TODO</div>;
+}
+
+export function Dropdown() {
+  return <div className="kth-0-dropdown">This is a dropdown</div>;
+}
--- @kth/style/src/components/Dropdown/index.tsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export function Dropdown() {
-  return <div className="kth-0-dropdown">This is a dropdown</div>;
-}
