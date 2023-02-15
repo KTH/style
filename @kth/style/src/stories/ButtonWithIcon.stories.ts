c62596571528f5fceadc5269fba11f03f35193fd
Carlos Saito <carlos@saito.style>
2023-02-15 11:07:35 +0100

    Include reset in stories

---
+++ @kth/style/src/stories/ButtonWithIcon.stories.ts
@@ -0,0 +1,18 @@
+import type { Meta, StoryObj } from "@storybook/react";
+
+import { ButtonWithIcon } from "../components/BaseButton";
+import "../../scss/reset.scss";
+import "../../scss/components/Button.scss";
+
+// More on how to set up stories at: https://storybook.js.org/docs/7.0/react/writing-stories/introduction
+const meta = {
+  title: "Example/ButtonWithIcon",
+  component: ButtonWithIcon,
+  tags: ["autodocs"],
+} satisfies Meta<typeof ButtonWithIcon>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+// More on writing stories with args: https://storybook.js.org/docs/7.0/react/writing-stories/args
+export const Primary: Story = {};
