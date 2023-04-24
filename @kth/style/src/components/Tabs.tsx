d7c1a2ad954cd30036dfdd7467d07394a2e5b997
Carlos Saito <carlos@saito.style>
2023-04-24 12:12:52 +0200

    Tabs

--- @kth/style/src/components/Tabs.tsx
+++ @kth/style/src/components/Tabs.tsx
@@ -1,16 +1,39 @@
 import React from "react";
 
-const TabsContext = React.createContext({
-  activeTab: "",
-});
+export function NavigationTabs({
+  children,
+}: {
+  children: React.ReactElement<TabProps>[];
+}) {
+  const containerRef = React.useRef<HTMLDivElement>(null);
+  const [activeTab, setActiveTab] = React.useState(0);
 
-export function NavigationTabs({ children }: { children: React.ReactNode }) {
-  return <div>{children}</div>;
+  return (
+    <div ref={containerRef} className="kth-tabs">
+      <ul className="kth-tabs__tablist">
+        {children.map((child, index) => (
+          <li key={child.props.id}>
+            <button
+              className="kth-tabs__tab"
+              onClick={() => {
+                setActiveTab(index);
+              }}
+            >
+              {child.props.title}
+            </button>
+          </li>
+        ))}
+      </ul>
+      {children[activeTab]}
+    </div>
+  );
 }
 
 interface TabProps {
   children: React.ReactNode;
+  id: string;
+  title: string;
 }
-export function Tab({ children }: TabProps) {
-  return <div>{children}</div>;
+export function Tab({ children, id, title }: TabProps) {
+  return <div id={id}>{children}</div>;
 }
--- @kth/style/src/stories/Tabs.stories.tsx
+++ @kth/style/src/stories/Tabs.stories.tsx
@@ -1,7 +1,9 @@
 import React from "react";
 import type { Meta, StoryObj } from "@storybook/react";
-
 import { Tab, NavigationTabs } from "../components/Tabs";
+
+import "../../scss/globals.scss";
+import "../../scss/components/tabs.scss";
 
 // More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction
 const meta = {
@@ -16,6 +18,13 @@ type Story = StoryObj<typeof meta>;
 // More on writing stories with args: https://storybook.js.org/docs/react/writing-stories/args
 export const Primary: Story = {
   args: {
-    children: <Tab>Tab 1</Tab>,
+    children: [
+      <Tab id="tab1" title="Tab 1">
+        Tab 1
+      </Tab>,
+      <Tab id="tab2" title="Tab 2">
+        Tab 2
+      </Tab>,
+    ],
   },
 };
