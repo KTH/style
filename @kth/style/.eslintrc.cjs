7ac114bd1135f0a7a6e2142047c86e2c473a5a5c
Carlos Saito <carlos@saito.style>
2023-04-24 14:51:07 +0200

    Simplify eslint rules

--- @kth/style/.eslintrc.cjs
+++ @kth/style/.eslintrc.cjs
@@ -8,15 +8,10 @@ module.exports = {
     "eslint:recommended",
     "plugin:@typescript-eslint/recommended",
     "plugin:react-hooks/recommended",
-    "plugin:storybook/recommended",
   ],
   parser: "@typescript-eslint/parser",
   parserOptions: {
     ecmaVersion: "latest",
     sourceType: "module",
-  },
-  plugins: ["react-refresh"],
-  rules: {
-    "react-refresh/only-export-components": "warn",
   },
 };
