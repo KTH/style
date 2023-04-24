9537983175bb162391ee7ad8336e6457c2538629
Carlos Saito <carlos@saito.style>
2023-04-24 14:27:44 +0200

    Attempt of query parameters with popstate

--- @kth/style/src/components/Tabs.tsx
+++ @kth/style/src/components/Tabs.tsx
@@ -20,20 +20,54 @@ function useUrlHash() {
   return currentHash;
 }
 
+/** Get the value of the given `param` in the current URL */
+function useUrlQuery(param: string) {
+  const [value, setValue] = React.useState("");
+
+  React.useEffect(() => {
+    function updateValue() {
+      const url = new URL(location.href);
+      console.log(url);
+      const value = url.searchParams.get(param) || "";
+      setValue(value);
+    }
+
+    window.addEventListener("popstate", updateValue);
+    window.addEventListener("pushstate", updateValue);
+    updateValue();
+
+    return () => {
+      window.removeEventListener("popstate", updateValue);
+      window.removeEventListener("pushstate", updateValue);
+    };
+  }, [param]);
+
+  return value;
+}
+
 export function ContentTabs({
+  id,
   children,
   url = "hash",
   defaultValue,
 }: {
+  id: string;
   children: React.ReactElement<TabProps>[];
   url?: "hash" | "query" | "none";
   defaultValue?: string;
 }) {
   const containerRef = React.useRef<HTMLDivElement>(null);
   const [manualActiveTab, setManualActiveTab] = React.useState("");
-  const hashActiveTab = useUrlHash() || defaultValue;
+  const hashActiveTab = useUrlHash() || defaultValue || "";
+  const queryActiveTab = useUrlQuery(id) || defaultValue || "";
 
-  const activeTab = url === "hash" ? hashActiveTab : manualActiveTab;
+  let activeTab = manualActiveTab;
+
+  if (url === "hash") {
+    activeTab = hashActiveTab;
+  } else if (url === "query") {
+    activeTab = queryActiveTab;
+  }
 
   let activeTabIndex = children.findIndex((c) => c.props.id === activeTab);
   if (activeTabIndex === -1) {
@@ -46,13 +80,23 @@ export function ContentTabs({
         {children.map((child, index) => (
           <li key={child.props.id}>
             <a
-              href={`#${child.props.id}`}
+              href={
+                url === "hash"
+                  ? `#${child.props.id}`
+                  : `?${id}=${child.props.id}`
+              }
               className="kth-tabs__tab"
               aria-selected={index === activeTabIndex}
               onClick={(event) => {
                 if (url === "none") {
                   event.preventDefault();
                   setManualActiveTab(child.props.id);
+                } else if (url === "query") {
+                  event.preventDefault();
+                  const url = new URL(location.href);
+                  url.searchParams.set(id, child.props.id);
+
+                  history.pushState(undefined, "", url);
                 }
               }}
             >
