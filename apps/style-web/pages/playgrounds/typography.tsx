a1c106d24f021630e4d453bda46b2917d5904a3d
Carlos Saito <carlos@saito.style>
2023-05-02 16:54:16 +0200

    Style almost all tags

--- apps/style-web/pages/playgrounds/typography.tsx
+++ apps/style-web/pages/playgrounds/typography.tsx
@@ -47,19 +47,6 @@ export default function TypographyPage() {
         vanilla HTML content and turn it into a beautiful, well-formatted
         document:
       </p>
-      <pre>
-        <code className="language-html">
-          &lt;article className="prose"&gt; &lt;h1&gt;Garlic bread with cheese:
-          What the science tells us&lt;/h1&gt; &lt;p&gt; For years parents have
-          espoused the health benefits of eating garlic bread with cheese to
-          their children, with the food earning such an iconic status in our
-          culture that kids will often dress up as warm, cheesy loaf for
-          Halloween. &lt;/p&gt; &lt;p&gt; But a recent study shows that the
-          celebrated appetizer may be linked to a series of rabies cases
-          springing up around the country. &lt;/p&gt; &lt;!-- ... --&gt;
-          &lt;/article&gt;
-        </code>
-      </pre>
       <p>
         For more information about how to use the plugin and the features it
         includes,{" "}
