d07441f2ca668a12b9758f328bcb911bdba6eac7
Carlos Saito <carlos@saito.style>
2023-05-04 16:05:56 +0200

    Move server.ts outside of "src"

--- apps/style-web/src/pages/_app.tsx
+++ apps/style-web/src/pages/_app.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import type { AppProps } from "next/app";
-import "../css/index.scss";
+import "@/css/index.scss";
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
