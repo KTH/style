5fc6111d093d701e64cfcc7591f881b845f53169
Carlos Saito <carlos@saito.style>
2023-05-04 15:07:31 +0200

    Generate cjs output

--- @kth/style/rollup.config.mjs
+++ @kth/style/rollup.config.mjs
@@ -1,9 +1,14 @@
+import typescript from "@rollup/plugin-typescript";
+
 export default [
   {
-    input: "src/index.ts",
-    output: {
-      file: "dist/cjs/index.js",
-      format: "cjs",
-    },
+    input: "src/index.js",
+    output: [
+      {
+        file: "dist/cjs/index.js",
+        format: "cjs",
+      },
+    ],
+    plugins: [typescript()],
   },
 ];
