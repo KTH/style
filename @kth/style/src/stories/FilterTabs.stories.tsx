7788c2059a72e8dae319ecba0b25fa6c075b0bcb
Carlos Saito <carlos@saito.style>
2023-05-22 10:00:55 +0200

    Change navigation-tabs to filter-tabs

--- @kth/style/src/stories/FilterTabs.stories.tsx
+++ @kth/style/src/stories/FilterTabs.stories.tsx
@@ -1,16 +1,16 @@
 import React from "react";
 import type { Meta, StoryObj } from "@storybook/react";
-import { Tab, NavigationTabs } from "../components/Tabs";
+import { FilterTabs, Radio } from "../components/Radio";
 
 import "../../scss/globals.scss";
-import "../../scss/components/navigation-tabs.scss";
+import "../../scss/components/filter-tabs.scss";
 
 // More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction
 const meta = {
-  title: "KTH Style/Navigation Tabs",
-  component: NavigationTabs,
+  title: "KTH Style/Filter Tabs",
+  component: FilterTabs,
   tags: ["autodocs"],
-} satisfies Meta<typeof NavigationTabs>;
+} satisfies Meta<typeof FilterTabs>;
 
 export default meta;
 type Story = StoryObj<typeof meta>;
@@ -18,15 +18,12 @@ type Story = StoryObj<typeof meta>;
 // More on writing stories with args: https://storybook.js.org/docs/react/writing-stories/args
 export const Primary: Story = {
   args: {
-    id: "my-tabs",
-    url: "query",
+    value: "1",
+    // eslint-disable-next-line @typescript-eslint/no-empty-function
+    onChange: () => {},
     children: [
-      <Tab id="tab1" title="Tab 1">
-        Tab 1
-      </Tab>,
-      <Tab id="tab2" title="Tab 2">
-        Tab 2
-      </Tab>,
+      <Radio key="1" value="1" label="Radio 1" />,
+      <Radio key="2" value="2" label="Radio 2" />,
     ],
   },
 };
