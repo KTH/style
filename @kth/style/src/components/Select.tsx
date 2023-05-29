1c1d6ca23d50508d4f28bd40d2ec44384ce4a0d7
Carlos Saito <carlos@saito.style>
2023-05-29 15:43:41 +0200

    Add <Select> component

---
+++ @kth/style/src/components/Select.tsx
@@ -0,0 +1,80 @@
+import React from "react";
+
+const SelectContext = React.createContext<string>("");
+
+interface SelectProps {
+  /** HTML form `name` */
+  name: string;
+
+  /** Label for the select */
+  label: string;
+
+  /** Select description */
+  description?: string;
+
+  /** Current value of the select field */
+  value: string;
+
+  /** Called when the select value changes */
+  onChange(value: string): void;
+
+  /** Options */
+  children:
+    | React.ReactElement<OptionProps | OptionGroupProps>
+    | React.ReactElement<OptionProps | OptionGroupProps>[];
+}
+
+interface OptionProps {
+  /** The value for the option */
+  value: string;
+
+  /** Human-readable label for the option */
+  children: string;
+}
+
+interface OptionGroupProps {
+  label: string;
+  children: React.ReactNode;
+}
+
+export function Select({
+  name,
+  label,
+  description,
+  value,
+  onChange,
+  children,
+}: SelectProps) {
+  const id = `select-${name}`;
+  return (
+    <SelectContext.Provider value={value}>
+      <div className="kth-select">
+        <label htmlFor={id}>{label}</label>
+        {description && <p id={`${id}-description`}>{description}</p>}
+        <select
+          id={id}
+          aria-describedby={description && `${id}-description`}
+          name={name}
+          value={value}
+          onChange={(e) => onChange(e.target.value)}
+        >
+          {children}
+        </select>
+      </div>
+    </SelectContext.Provider>
+  );
+}
+
+export function OptionGroup({ label, children }: OptionGroupProps) {
+  return <optgroup label={label}>{children}</optgroup>;
+}
+
+export function Option({ value, children }: OptionProps) {
+  const selectedValue = React.useContext(SelectContext);
+
+  return (
+    <option value={value} selected={value === selectedValue}>
+      {children}
+    </option>
+  );
+}
---
+++ @kth/style/src/stories/Select.stories.tsx
@@ -0,0 +1,43 @@
+import React from "react";
+import type { Meta, StoryObj } from "@storybook/react";
+import { Select, Option, OptionGroup } from "../components/Select";
+
+import "../../scss/globals.scss";
+import "../../scss/components/select.scss";
+
+// More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction
+const meta = {
+  title: "KTH Style/Select",
+  component: Select,
+  tags: ["autodocs"],
+} satisfies Meta<typeof Select>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+// More on writing stories with args: https://storybook.js.org/docs/react/writing-stories/args
+export const Primary: Story = {
+  args: {
+    name: "example-select",
+    label: "Favorite dessert",
+    description:
+      "Select your favorite dessert. You can choose a healthy option or a sweet one. There are many options to choose from.",
+    value: "none",
+    // eslint-disable-next-line @typescript-eslint/no-empty-function
+    onChange: () => {},
+    children: [
+      <Option value="none">Select an option</Option>,
+      <Option value="ice">Ice cream</Option>,
+      <Option value="tiramisu">Tiramisu</Option>,
+      <Option value="cinnamon">Cinammon rolls</Option>,
+      <Option value="cake">Cake</Option>,
+      <Option value="sushi">Sushi</Option>,
+      <Option value="chocolate">Chocolate</Option>,
+      <OptionGroup label="Healty options">
+        <Option value="apple">Apple</Option>
+        <Option value="carrot">Carrot</Option>
+        <Option value="sushi">Sushi</Option>
+      </OptionGroup>,
+    ],
+  },
+};
