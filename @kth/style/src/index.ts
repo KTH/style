45104b007b6c1e8601d926ea64cb654a2e0c7696
Carlos Saito <carlos@saito.style>
2023-06-19 16:28:34 +0200

    Export icons

--- @kth/style/src/index.ts
+++ @kth/style/src/index.ts
@@ -1,2 +1,5 @@
 export { Tab, ContentTabs, NavigationTabs } from "./components/Tabs";
 export { Select, Option, OptionGroup } from "./components/Select";
+
+export { default as IconArrowBack } from "./icons/400/ArrowBack";
+export { default as IconCheck } from "./icons/400/Check";
