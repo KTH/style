1bf5750f8396bbadc02c2e38f072dfeb9a94facf
Carlos Saito <carlos@saito.style>
2023-08-14 13:21:44 +0200

    Export BackButton, Button, ForwardButton

--- @kth/style/src/index.ts
+++ @kth/style/src/index.ts
@@ -1,5 +1,6 @@
 export { Tab, ContentTabs, NavigationTabs } from "./components/Tabs";
 export { Select, Option, OptionGroup } from "./components/Select";
+export { BackButton, Button, ForwardButton } from "./components/Button";
 
 export { default as IconArrowBack } from "./icons/400/ArrowBack";
 export { default as IconCheck } from "./icons/400/Check";
