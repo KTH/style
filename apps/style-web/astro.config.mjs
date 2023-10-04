4de18df7aef0a67857e729cc30c0980e66c1a3c7
Carlos Saito <carlos@saito.style>
2023-10-04 09:30:33 +0200

    Compress files when building kth style web

--- apps/style-web/astro.config.mjs
+++ apps/style-web/astro.config.mjs
@@ -1,8 +1,10 @@
 import { defineConfig } from "astro/config";
+import compressor from "astro-compressor";
 
 // https://astro.build/config
 export default defineConfig({
   build: {
     assets: "style/_astro",
   },
+  integrations: [compressor()],
 });
