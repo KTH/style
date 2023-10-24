5b73a04678d82cd1b1133dd0714e95c3a56af3a8
Carlos Saito <carlos@saito.style>
2023-10-24 13:13:55 +0200

    Copy MenuPanel behavior in DropdownWithPanel

--- @kth/style/src/components/react/DropdownWithPanel.tsx
+++ @kth/style/src/components/react/DropdownWithPanel.tsx
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { KeyboardEvent } from "react";
+import { closeAllDialogs } from "../MenuPanel";
 
 interface Props {
   label: string;
@@ -8,6 +9,17 @@ interface Props {
 export function DropdownWithPanel({ label, children }: Props) {
   const [shouldRender, setShouldRender] = React.useState(false);
   const dialogRef = React.useRef<HTMLDialogElement>(null);
+
+  function closeDialog() {
+    dialogRef.current?.close();
+  }
+
+  function handleKeyDown(event: KeyboardEvent) {
+    if (event.key === "Escape") {
+      event.stopPropagation();
+      dialogRef.current?.close();
+    }
+  }
 
   return (
     <div>
@@ -22,9 +34,35 @@ export function DropdownWithPanel({ label, children }: Props) {
       >
         {label}
       </button>
-      <dialog className="kth-menu-panel" ref={dialogRef}>
-        {shouldRender && children}
+      <dialog
+        className="kth-menu-panel"
+        ref={dialogRef}
+        onKeyDown={handleKeyDown}
+      >
+        <div className="kth-menu-panel__container">
+          <button className="kth-button close" onClick={() => closeDialog()}>
+            <span className="kth-visually-hidden">Close</span>
+          </button>
+          <div className="kth-menu-panel__content">
+            {shouldRender && children}
+          </div>
+        </div>
       </dialog>
     </div>
+  );
+}
+
+export function DropdownContainer(props: React.HTMLAttributes<HTMLDivElement>) {
+  return (
+    <div
+      onBlur={(event) => {
+        const relatedTarget = event.relatedTarget;
+
+        if (relatedTarget && !event.target.contains(relatedTarget)) {
+          closeAllDialogs();
+        }
+      }}
+      {...props}
+    />
   );
 }
