953a1625f2fc8f4c91c6ed2b602e768b8c526134
Carlos Saito <carlos@saito.style>
2023-11-22 16:07:01 +0100

    Re-implement local navigation

---
+++ apps/style-web/src/i18n/local-navigation.ts
@@ -0,0 +1,58 @@
+type Entry =
+  | string
+  | {
+      heading: {
+        sv: string;
+        en: string;
+      };
+      routes: string[];
+    };
+
+export const navigationEntries: Entry[] = [
+  "",
+  {
+    heading: {
+      sv: "Kom igång",
+      en: "Get started",
+    },
+    routes: ["/get-started/developers"],
+  },
+  {
+    heading: {
+      sv: "Styles",
+      en: "Styles",
+    },
+    routes: [
+      "/styles/design-tokens",
+      "/styles/colors",
+      "/styles/typography",
+      "/styles/spacing",
+    ],
+  },
+  {
+    heading: {
+      sv: "Riktlinjer",
+      en: "Guides",
+    },
+    routes: [
+      "/guides/design",
+      "/guides/introduction",
+      "/guides/page-structure",
+    ],
+  },
+  {
+    heading: {
+      sv: "Komponenter",
+      en: "Components",
+    },
+    routes: [
+      "/components/details",
+      "/components/entrances",
+      "/components/header",
+      "/components/kpm",
+      "/components/logotype",
+      "/components/mega-menu",
+      "/components/menu-item",
+    ],
+  },
+];
