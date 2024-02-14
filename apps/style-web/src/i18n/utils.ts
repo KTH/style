b5098cb5048e25f00d5baf3b9b6a30bf8dd1308c
Carlos Saito <carlos@saito.style>
2024-02-14 17:32:56 +0100

    Refactor LocalNavigation

--- apps/style-web/src/i18n/utils.ts
+++ apps/style-web/src/i18n/utils.ts
@@ -1,3 +1,4 @@
+import type { MarkdownInstance } from "astro";
 import { ui, languages, defaultLang } from "./ui";
 import type { Languages } from "./ui";
 
@@ -38,5 +39,72 @@ export function getTranslatedLink(url: URL) {
 export function useTranslations(lang: Languages) {
   return function t(key: keyof (typeof ui)[typeof defaultLang]) {
     return ui[lang][key] || ui[defaultLang][key];
+  };
+}
+
+function getOriginalData(
+  allFiles: MarkdownInstance<Record<string, any>>[],
+  route: string,
+) {
+  const REGEX = /^\/style\/(\w+)(.*)$/;
+  const entry = allFiles
+    .filter(
+      (file) =>
+        file.frontmatter.original === undefined ||
+        file.frontmatter.original === true,
+    )
+    .find((file) => {
+      const match = file.url?.match(REGEX);
+
+      return `${match?.[2]}` === route;
+    });
+
+  if (entry && entry.url) {
+    const entryLanguage = entry.url.match(REGEX)?.[1];
+
+    if (entryLanguage !== "sv" && entryLanguage !== "en") {
+      throw new Error("");
+    }
+
+    return {
+      href: entry.url,
+      title: entry.frontmatter.title as string,
+      lang: entryLanguage,
+    };
+  }
+
+  throw new Error("");
+}
+
+export function getRouteData(
+  allFiles: MarkdownInstance<Record<string, any>>[],
+  lang: "sv" | "en",
+  route: string,
+) {
+  const entry = allFiles.find((file) => file.url === `/style/${lang}${route}`);
+
+  if (!entry) {
+    throw new Error("");
+  }
+
+  const status = entry?.frontmatter.status;
+  const original = entry?.frontmatter.original;
+
+  if (status === "empty" && original === false) {
+    return getOriginalData(allFiles, route);
+  }
+
+  if (!entry.url) {
+    throw new Error("");
+  }
+
+  if (typeof entry.frontmatter.title !== "string") {
+    throw new Error("");
+  }
+
+  return {
+    href: entry.url,
+    title: entry.frontmatter.title,
+    lang: undefined,
   };
 }
